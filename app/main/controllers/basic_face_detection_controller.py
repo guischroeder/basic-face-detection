b91815d89c1656dbb5577f1f8eb9a0204ab72b5c
guischroeder <guilherme.schroederr@gmail.com>
2021-03-16 22:06:50 -0300

    feat(error-handler): add generic error handling

--- app/main/controllers/basic_face_detection_controller.py
+++ app/main/controllers/basic_face_detection_controller.py
@@ -1,12 +1,18 @@
 import os
 
 from flask import Blueprint
+from app.settings import Settings
+from app.main.utils.handle_error import handle_error
 from app.main.services.hackattic_service import HackatticService
 from app.main.services.s3_service import S3Service
-from app.settings import Settings
 
 
 basic_face_detection_bp = Blueprint("basic_face_detection", __name__)
+
+
+@basic_face_detection_bp.app_errorhandler(Exception)
+def error_handler(e):
+    return handle_error(e)
 
 
 @basic_face_detection_bp.route("/problem", methods=["GET"])
--- app/main/services/s3_service.py
+++ app/main/services/s3_service.py
@@ -14,16 +14,14 @@ class S3Service:
         return self._put_object(
             {"Bucket": bucket, "Key": key, "Body": response.content}
         )
-    
+
     def _put_object(self, params):
         return self._get_instance().put_object(**params)
 
     def _get_instance(self):
         if not self._s3_client:
             self._s3_client = boto3.client(
-                "s3",
-                region_name="us-west-1",
-                **self._credentials
+                "s3", region_name="us-west-1", **self._credentials
             )
 
         return self._s3_client
