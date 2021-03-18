33d5efd00ee8035f60ad97f417dec16cc2c2624f
guischroeder <guilherme.schroederr@gmail.com>
2021-03-18 19:08:08 -0300

    refactor(s3_service): change to has an aws client injected

--- app/main/controllers/basic_face_detection_controller.py
+++ app/main/controllers/basic_face_detection_controller.py
@@ -2,9 +2,10 @@ import os
 
 from flask import Blueprint
 from app.settings import Settings
-from app.main.utils.handle_error import handle_error
+from app.main.clients.aws_client import AWSClient
 from app.main.services.hackattic_service import HackatticService
 from app.main.services.s3_service import S3Service
+from app.main.utils.handle_error import handle_error
 
 
 basic_face_detection_bp = Blueprint("basic_face_detection", __name__)
@@ -15,9 +16,16 @@ def error_handler(e):
     return handle_error(e)
 
 
-@basic_face_detection_bp.route("/problem", methods=["GET"])
+@basic_face_detection_bp.route("/solve", methods=["GET"])
 def get():
+    s3_client = AWSClient(
+        aws_service="s3",
+        region_name="us-west-1",
+        credentials=Settings.aws_s3_credentials(),
+    )
+
     image_url = HackatticService(Settings.hackattic_credentials()).get_image_url()
-    return S3Service(Settings.aws_s3_credentials()).upload_image_from_url(
+
+    return S3Service(s3_client).upload_image_from_url(
         image_url, os.getenv("S3_BUCKET"), "media/faces.jpg"
     )
--- app/main/services/s3_service.py
+++ app/main/services/s3_service.py
@@ -1,12 +1,9 @@
-import boto3
 import requests
 
 
 class S3Service:
-    _s3_client = None
-
-    def __init__(self, credentials):
-        self._credentials = credentials
+    def __init__(self, aws_client):
+        self._aws_client = aws_client
 
     def upload_image_from_url(self, image_url, bucket, key):
         response = requests.get(image_url)
@@ -16,12 +13,4 @@ class S3Service:
         )
 
     def _put_object(self, params):
-        return self._get_instance().put_object(**params)
-
-    def _get_instance(self):
-        if not self._s3_client:
-            self._s3_client = boto3.client(
-                "s3", region_name="us-west-1", **self._credentials
-            )
-
-        return self._s3_client
+        return self._aws_client.get_instance().put_object(**params)
