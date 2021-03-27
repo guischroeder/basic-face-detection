d35a90e6644c55a23944528ecc246c23437ebb09
guischroeder <guilherme.schroederr@gmail.com>
2021-03-27 19:43:23 -0300

    test: aws client

--- app/main/libs/hackattic/tests/test_hackattic_service.py
+++ app/main/libs/hackattic/tests/test_hackattic_service.py
@@ -9,16 +9,14 @@ class TestHackatticService:
         cls._access_token = "test_token"
 
     def setup_method(self):
-        self._hackattic_service = HackatticService({"access_token": self._access_token })
+        self._hackattic_service = HackatticService({"access_token": self._access_token})
 
     def test_get_image_url(self, mocker):
         test_image = "http://test_image.com"
 
         mocker.patch(
             "requests.get",
-            return_value=mocker.Mock(
-                json=lambda : {"image_url": test_image },
-            )
+            return_value=mocker.Mock(json=lambda: {"image_url": test_image},),
         )
 
         assert self._hackattic_service.get_image_url() == test_image
@@ -30,15 +28,11 @@ class TestHackatticService:
         data = [[1, 2], [3, 4], [2, 1], [4, 3]]
 
         mocker.patch(
-            "requests.post",
-            return_value=mocker.Mock(
-                json=lambda : {"result": True },
-            )
-        )
-        
-        assert self._hackattic_service.solve(data) == { "result": True }
-        requests.post.assert_called_once_with(
-            f"https://hackattic.com/challenges/basic_face_detection/solve?access_token={self._access_token}",
-            {"face_tiles": data }
+            "requests.post", return_value=mocker.Mock(json=lambda: {"result": True},)
         )
 
+        assert self._hackattic_service.solve(data) == {"result": True}
+        requests.post.assert_called_once_with(
+            f"https://hackattic.com/challenges/basic_face_detection/solve?access_token={self._access_token}",
+            {"face_tiles": data},
+        )
